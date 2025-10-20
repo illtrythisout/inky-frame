4a429ffea9b14befb54df46a07636908abd62dd3
Theo <170449321+illtrythisout@users.noreply.github.com>
2025-10-20 18:56:48 +0200

    Create current album widget

--- frontend/src/components/Dashboard/CurrentAlbumCard.jsx
+++ frontend/src/components/Dashboard/CurrentAlbumCard.jsx
@@ -1,7 +1,18 @@
+import styles from './currentAlbumCard.module.css';
+
+import imageIcon from '../../assets/icons/image.svg';
+
 export default function CurrentAlbumCard() {
   return (
-    <div className="card">
-      <p>Current Album</p>
+    <div className={`card ${styles.widget}`}>
+      <div className={styles.imageContainer}>
+        <img src={imageIcon} alt="Current Album" />
+      </div>
+      <div className={styles.albumDescription}>
+        <p className={styles.albumName}>Travel</p>
+        <p className={styles.numberOfImages}>Image 12 of 13</p>
+      </div>
+      <p className={styles.description}>Current Album</p>
     </div>
   );
 }
