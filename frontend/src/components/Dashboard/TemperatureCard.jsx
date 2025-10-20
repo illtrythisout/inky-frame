e25577be253a81834feefeac88fad453c9c86dd0
Theo <170449321+illtrythisout@users.noreply.github.com>
2025-10-20 19:15:57 +0200

    Create temperatureCard

--- frontend/src/components/Dashboard/TemperatureCard.jsx
+++ frontend/src/components/Dashboard/TemperatureCard.jsx
@@ -1,7 +1,13 @@
+import styles from './temperatureCard.module.css';
+
 export default function TemperatureCard() {
+  let warningLevel = 'safe';
   return (
-    <div className="card">
-      <p>Temperature Card</p>
+    <div className={`card ${styles.widget}`}>
+      <div className={`${styles.imageContainer} ${styles[warningLevel]}`}>
+        <p>41C°</p>
+      </div>
+      <p>CPU Temperature</p>
     </div>
   );
 }
