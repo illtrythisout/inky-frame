70a8393b4db27f77489e53a0ed74536225d91e62
Theo <170449321+illtrythisout@users.noreply.github.com>
2025-11-01 12:07:42 +0100

    Add cors protection

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,23 @@
 const path = require('node:path');
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 
 const app = express(); // initialize express in app
+
+const allowedOrigins = ['http://localhost:5173'];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by cors'));
+      }
+    },
+  })
+);
 
 // Middleware to allow us to correctly parse the incoming request’s body
 app.use(express.urlencoded({ extended: true }));
