eb243c4894851bef4bae93e44f4944af5b6f70b5
Theo <170449321+illtrythisout@users.noreply.github.com>
2025-11-01 12:33:35 +0100

    Update album page to use api

--- frontend/src/pages/AlbumsPage.jsx
+++ frontend/src/pages/AlbumsPage.jsx
@@ -1,18 +1,24 @@
 import styles from './albumsPage.module.css';
+import { useOutletContext } from 'react-router';
+import { Link } from 'react-router';
+
 import AlbumCard from '../components/dashboard/AlbumCard';
 
 export default function AlbumsPage() {
+  const context = useOutletContext();
+  const albums = context.albums.data;
   return (
     <div className="card">
       <h1>All Albums</h1>
       <div className={styles.albums}>
-        <AlbumCard />
-        <AlbumCard />
-        <AlbumCard />
-        <AlbumCard />
-        <AlbumCard />
-        <AlbumCard />
-        <AlbumCard />
+        {!albums ||
+          albums.map((album) => {
+            return (
+              <Link to={`/albums/${album.id}`}>
+                <AlbumCard key={album.id} data={album} />
+              </Link>
+            );
+          })}
       </div>
     </div>
   );
