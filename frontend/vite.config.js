b8bef67ba7b1cb0f294e462f2fc1f885ae11d15b
Theo <170449321+illtrythisout@users.noreply.github.com>
2025-11-09 01:33:04 +0100

    Added env variable to allow specific hosts for vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// get env variables
+const ALLOWED_HOSTS = import.meta.env.VITE_ALLOWED_HOSTS;
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,
+    allowedHosts: [ALLOWED_HOSTS],
+  },
+});
