878ffd019fe19c2d5e15bab1aa4fd982d1b3d7dc
Theo <170449321+illtrythisout@users.noreply.github.com>
2025-11-09 22:07:52 +0100

    Call api in temperature card

--- frontend/src/components/Dashboard/TemperatureCard.jsx
+++ frontend/src/components/Dashboard/TemperatureCard.jsx
@@ -1,11 +1,43 @@
 import styles from './temperatureCard.module.css';
+import { useEffect, useState } from 'react';
+
+// get env variables
+const API_URL = import.meta.env.VITE_API_URL;
 
 export default function TemperatureCard() {
-  let warningLevel = 'safe';
+  const [warningLevel, setWarningLevel] = useState('safe');
+  const [temperature, setTemperature] = useState();
+
+  useEffect(() => {
+    async function fetchTemp() {
+      const res = await fetch(`${API_URL}/display/pi/temp`);
+      const data = await res.json();
+      setTemperature(data.data.temperature);
+
+      if (temperature >= 0 && temperature < 60) {
+        // safe temp between 0 and 60 C
+        setWarningLevel('safe');
+      } else if (
+        // warning temp between 60 and 80 C or between -20 and 0 C
+        (temperature < 0 && temperature >= -20) ||
+        (temperature <= 60 && temperature > 80)
+      ) {
+        setWarningLevel('warning');
+      } else {
+        // dangerous temp greater than 80 C or less than -20 C
+        setWarningLevel('danger');
+      }
+    }
+
+    fetchTemp();
+    const interval = setInterval(fetchTemp, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className={`card ${styles.widget}`}>
       <div className={`${styles.imageContainer} ${styles[warningLevel]}`}>
-        <p>41C°</p>
+        <p>{temperature ? `${temperature}C°` : '--C°'}</p>
       </div>
       <h3>CPU Temperature</h3>
     </div>
